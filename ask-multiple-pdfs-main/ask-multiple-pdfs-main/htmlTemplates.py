76868e1345aa510cf7925a7ee5d1e01174681a1e
senapathisowjanya <sowjanyanageswar9@gmail.com>
2023-09-13 12:43:08 +0530

    done

--- ask-multiple-pdfs-main/ask-multiple-pdfs-main/htmlTemplates.py
+++ ask-multiple-pdfs-main/ask-multiple-pdfs-main/htmlTemplates.py
@@ -1,38 +1,43 @@
-# Define CSS styles separately
+# Define CSS styles
 css = '''
 <style>
 .chat-message {
-    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+    padding: 1.5rem;
+    border-radius: 0.5rem;
+    margin-bottom: 1rem;
+    display: flex;
 }
 .chat-message.user {
-    background-color: #2b313e
+    background-color: #2b313e;
 }
 .chat-message.bot {
-    background-color: #475063
+    background-color: #475063;
 }
-.chat-message .avatar {
-  width: 20%;
+.chat-message.user .avatar {
+    width: 20%;
+}
+.chat-message.bot .avatar {
+    width: 20%;
 }
 .chat-message .avatar img {
-  max-width: 78px;
-  max-height: 78px;
-  border-radius: 50%;
-  object-fit: cover;
+    max-width: 78px;
+    max-height: 78px;
+    border-radius: 50%;
+    object-fit: cover;
 }
 .chat-message .message {
-  width: 80%;
-  padding: 0 1.5rem;
-  color: #fff;
+    width: 80%;
+    padding: 0 1.5rem;
+    color: #fff;
 }
 </style>
 '''
 
-# Define your HTML templates with the correct image URLs
+# Define your HTML templates with unique avatar classes
 bot_template = '''
 <div class="chat-message bot">
-    <div class="avatar">
-         <img src="https://dataconomy.com/wp-content/uploads/2022/12/OpenAI-ChatGPT-New-chatbots-examples-limitations-and-more-6-1536x1009.jpg" 
-        style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+    <div class="avatar bot-avatar">
+        <img src="https://dataconomy.com/wp-content/uploads/2022/12/OpenAI-ChatGPT-New-chatbots-examples-limitations-and-more-6-1536x1009.jpg">
     </div>
     <div class="message">{{MSG}}</div>
 </div>
@@ -40,10 +45,11 @@ bot_template = '''
 
 user_template = '''
 <div class="chat-message user">
-    <div class="avatar">
-        <img src="https://cdn.dribbble.com/users/1937255/screenshots/14588207/media/0d0b9719cb11ea243c7c9a67ac97dc47.png"
-        style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
-    </div>    
+    <div class="avatar user-avatar">
+        <img src="https://cdn.dribbble.com/users/1937255/screenshots/14588207/media/0d0b9719cb11ea243c7c9a67ac97dc47.png">
+    </div>
     <div class="message">{{MSG}}</div>
 </div>
 '''
+
+# Rest of your code remains the same
